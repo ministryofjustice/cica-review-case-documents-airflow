e62d7f99a27cab27493401c9132e66472e3f1c3a
Marco Surace <marco.surace@justice.gov.uk>
2025-07-11 10:28:24 +0100

    added modules for ingestion

--- document-search-airflow/src/document_search_airflow/index_document.py
+++ document-search-airflow/src/document_search_airflow/index_document.py
@@ -5,21 +5,21 @@ from opensearchpy import OpenSearch
 
 # --- Configuration ---
 load_dotenv()
-domain_name = os.getenv("DOMAIN_NAME")
-aws_region = os.getenv("AWS_REGION")
-port = os.getenv("OPENSEARCH_PORT")
-index_name = os.getenv("INDEX_NAME")
-opensearch_host = f"{domain_name}.{aws_region}.opensearch.localhost.localstack.cloud"
-opensearch_username = os.getenv("OPENSEARCH_USERNAME")
-opensearch_password = os.getenv("OPENSEARCH_PASSWORD")
+DOMAIN_NAME = os.getenv("DOMAIN_NAME")
+AWS_REGION = os.getenv("AWS_REGION")
+OPENSEARCH_PORT = os.getenv("OPENSEARCH_PORT")
+INDEX_NAME = os.getenv("INDEX_NAME")
+OPENSEARCH_HOST = f"{DOMAIN_NAME}.{AWS_REGION}.opensearch.localhost.localstack.cloud"
+OPENSEARCH_USERNAME = os.getenv("OPENSEARCH_USERNAME")
+OPENSEARCH_PASSWORD = os.getenv("OPENSEARCH_PASSWORD")
 
 # --- OpenSearch Client Configuration ---
 # For a local OpenSearch instance, typically you'd use HTTP and basic auth.
 # If you are using AWS OpenSearch Service, you would need AWS4Auth.
 # For simplicity with a local docker-compose setup, we'll use basic auth.
-auth = (opensearch_username, opensearch_password)
+auth = (OPENSEARCH_USERNAME, OPENSEARCH_PASSWORD)
 client = OpenSearch(
-    hosts=[{"host": opensearch_host, "port": port}],
+    hosts=[{"host": OPENSEARCH_HOST, "port": OPENSEARCH_PORT}],
     http_auth=auth,
     http_compress=True,  # enables gzip compression for request bodies
     use_ssl=True,  # Use SSL for local OpenSearch, usually self-signed
@@ -32,11 +32,11 @@ client = OpenSearch(
 document = {"title": "Moneyball", "director": "Bennett Miller", "year": "2011"}
 
 # -- Index a document
-print(f"Checking if index '{index_name}' already exists...")
-if client.indices.exists(index=index_name):
-    print(f"Index '{index_name}' exists.")
+print(f"Checking if index '{INDEX_NAME}' already exists...")
+if client.indices.exists(index=INDEX_NAME):
+    print(f"Index '{INDEX_NAME}' exists.")
     try:
-        response = client.index(index=index_name, body=document, id="1", refresh=True)
+        response = client.index(index=INDEX_NAME, body=document, id="1", refresh=True)
         print("Document successfully indexed.")
         print(response)
     except Exception as e:
