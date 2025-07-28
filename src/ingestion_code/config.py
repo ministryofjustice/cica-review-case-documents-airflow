b6e7454744edb59791c38bc54cfbaf109472da9c
Marco Surace <marco.surace@justice.gov.uk>
2025-07-28 14:17:23 +0100

    initial commit for ingestion with ocr

--- src/ingestion_code/config.py
+++ src/ingestion_code/config.py
@@ -17,6 +17,11 @@ class Settings(BaseSettings):  # type: ignore
     OPENSEARCH_USERNAME: str = "username"
     OPENSEARCH_PASSWORD: str = "password"
     OPENSEARCH_INDEX_NAME: str = "case-documents"
+    AWS_ACCESS_KEY_ID: str = "aws_access_key_id"
+    AWS_SECRET_ACCESS_KEY: str = "aws_secret_access_key"
+    AWS_SESSION_TOKEN: str = "aws_session_token"
+    AWS_REGION: str = "aws_region"
+    S3_BUCKET_NAME: str = "s3_bucket_name"
 
 
 settings = Settings()
