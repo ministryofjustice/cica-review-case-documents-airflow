44d0fce5453a14b306c50c78e4a14fedcbdab956
Marco Surace <marco.surace@justice.gov.uk>
2025-08-11 16:12:46 +0100

    fixed bugs

--- src/ingestion_code/__main__.py
+++ src/ingestion_code/__main__.py
@@ -5,14 +5,15 @@ from ingestion_code.chunk_text import (
 )
 from ingestion_code.config import settings
 from ingestion_code.embed_text import (
-    embed_text_with_cohere_model,
+    # embed_text_with_cohere_model,
+    embed_text_with_titan_model,
 )
 from ingestion_code.index_text import index_textract_text_to_opensearch
 
 # from ingestion_code.extract_text import extract_text_from_pdf
 # from ingestion_code.chunk_text import chunk_text_for_local_model
 # from ingestion_code.embed_text import embed_text_with_local_model
-from ingestion_code.ocr_text import TextractMode, run_textract_and_upload_responses
+# from ingestion_code.ocr_text import TextractMode, run_textract_and_upload_responses
 from ingestion_code.utils import load_local_jsons, load_s3_jsons
 
 logging.basicConfig(
@@ -46,26 +47,31 @@ def main():
 
     # -- Ingesting PDF requiring OCR to extract text --
 
-    # -- Step 1: OCR PDFs with Textract and save JSON responses to S3 --
     # -- Run on AP --
-    run_textract_and_upload_responses(
-        settings.S3_BUCKET_NAME, settings.S3_PREFIX, TextractMode.TEXT_DETECTION
-    )
+    # -- Step 1: OCR PDFs with Textract and save JSON responses to S3 --
+    # run_textract_and_upload_responses(
+    #     settings.S3_BUCKET_NAME, settings.S3_PREFIX, TextractMode.TEXT_DETECTION
+    # )
 
+    # -- Run on AP or local machine --
     # -- Step 2: Load Textract responses and split text into chunks --
-    folder_name = settings.S3_PREFIX + "/text-detection"
     if settings.LOCAL:
-        responses = load_local_jsons(folder_name)
+        responses = load_local_jsons("redacted-black")
     else:
-        responses = load_s3_jsons(settings.S3_BUCKET_NAME, folder_name)
+        responses = load_s3_jsons(
+            settings.S3_BUCKET_NAME, settings.S3_PREFIX + "/text-detection"
+        )
     chunks = chunk_textract_responses_for_bedrock(responses)
 
+    # -- Run on AP or local machine --
     # -- Step 3: Create chunk embeddings --
-    # -- Use AP or Mod Platform sandbox Bedrock --
-    chunks_with_embeddings = embed_text_with_cohere_model(chunks)
+    # -- Cohere model --
+    # chunks_with_embeddings = embed_text_with_cohere_model(chunks)
+    # -- Or, Titan model --
+    chunks_with_embeddings = embed_text_with_titan_model(chunks)
 
-    # -- Step 4: Index documents to OpenSearch --
-    # -- First activate LocalStack set up, then run locally
+    # -- Run on local machine --
+    # -- Step 4: Index documents to LocalStack OpenSearch --
     index_textract_text_to_opensearch(chunks_with_embeddings)
 
     logger.info("Script finished.")
