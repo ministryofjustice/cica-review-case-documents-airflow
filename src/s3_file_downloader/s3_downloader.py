7789e8c291b7e36fb80d81e45a8bd87c960321d4
neil-stephen-mcgonigle <neil.mcgonigle@justice.gov.uk>
2025-08-14 13:40:26 +0100

    Add s3_downloader function

---
+++ src/s3_file_downloader/s3_downloader.py
@@ -0,0 +1,25 @@
+import logging
+
+import boto3
+
+logger = logging.getLogger(__name__)
+
+
+def download_pdf_from_s3(bucket_name: str, file_key: str, download_path: str):
+    """
+    Downloads a PDF file from an S3 bucket.
+
+    Args:
+        bucket_name (str): The name of the S3 bucket.
+        file_key (str): The key (path) of the file in the bucket.
+        download_path (str): The local path to save the downloaded file.
+
+    Raises:
+        botocore.exceptions.ClientError: If the file is not found
+        or another S3 error occurs.
+    """
+    s3 = boto3.client("s3")
+    s3.download_file(bucket_name, file_key, download_path)
+    logger.info(
+        f"Successfully downloaded {file_key} from bucket {bucket_name} to {download_path}"
+    )
