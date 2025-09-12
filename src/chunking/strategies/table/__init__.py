c3d4b736b95069782b84cc4977c6e55dfd7a2dde
neil-stephen-mcgonigle <neil.mcgonigle@justice.gov.uk>
2025-09-12 09:29:11 +0100

    Tidy up comments

--- src/chunking/strategies/table/__init__.py
+++ src/chunking/strategies/table/__init__.py
@@ -1,5 +1,3 @@
-# src/chunking/strategies/table/__init__.py
-
 """
 Table chunking strategies package.
 
@@ -8,14 +6,9 @@ For example, other modules can now use:
 `from src.chunking.strategies.table import LayoutTableChunkingStrategy`
 """
 
-# Expose the main strategy class from its submodule
 from .layout_table_strategy import LayoutTableChunkingStrategy
-
-# Also expose the relevant data types from their submodule
 from .schemas import TextBlock
 
-# It's good practice to also define __all__ to control `from ... import *`
-# and to make the public API of the package explicit.
 __all__ = [
     "LayoutTableChunkingStrategy",
     "TextBlock",
